edfa3fda40c17b8448f45d5a6fe2677c9cea72fa
Sergio Pavanelli <sergiopavanelli@MacBook-Pro-de-Sergio.local>
2024-05-21 17:35:51 -0300

    update_05

--- LoginSistema/src/LoginSistema.java
+++ LoginSistema/src/LoginSistema.java
@@ -1,7 +1,7 @@
 public class LoginSistema {
 
     
-    protected static final String correctEmail = "sergiopavanelli@gmail.com";
+    protected static final String correctEmail = "logtest@gmail.com";
     protected static final String correctPassword = "1234";
     protected static final String logFile = "log.txt";
 
